2bf9f519bb6167153d618c0d14c9772df99eba5e
pc <ministove3yo@gmail.com>
2022-06-11 00:32:54 +0900

    2022-06-10

--- 22.py
+++ 22.py
@@ -1,19 +1,14 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        sets = {""}
-        for i in range(n):
-            temp = set()
-            for elem in sets:
-                temp.add(f"(){elem}")
-                temp.add(f"({elem})")
-                temp.add(f"{elem}()")
-            sets = temp
-        return sorted(list(sets), reverse=False)
-1| 1
-  ()
-2| 1 1, 0.5 1 0.5
-  ()() (())
-3| 1 1 1, 0.5 1 1 0.5, 1 2, 2 1, 3
-  ()()() (()()) ()(()) (())() ((()))
-4| 1 1 1 1, 1 1 2,    1 2 1,   2 1 1,    1 3,    2 2,     3 1,      4
-  ()()()() ()()(()) ()(())() (())()() ()((())) (())(()) ((()))() (((())))
+        sets = []
+
+        def back(current, left, right):
+            if left + right == n * 2:
+                sets.append(current)
+            if left < n:
+                back(current + "(", left + 1, right)
+            if right < left:
+                back(current + ")", left, right + 1)
+
+        back("", 0, 0)
+        return sets
