a00edcfb4808ef0f1c29b7877d6eea855c55d456
pc <ministove3yo@gmail.com>
2022-06-17 22:49:10 +0900

    2022-06-17

--- 15.py
+++ 15.py
@@ -1,26 +1,24 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        hashNum = {}
-        sortedNums = sorted(nums)
-        for i in sortedNums:
-            hashNum[i] = hashNum.get(i, 0) + 1
+        DP = {}
         answer = set()
-        fuckduplicate = {}
-        def recursiveRight(dic, left, right):
-            if right >= len(nums):
-                return
-            dic[sortedNums[right]] -= 1
-            if dic.get(-(sortedNums[left] + sortedNums[right]), 0):
-                answer.add(tuple([sortedNums[left], sortedNums[right], -(sortedNums[left] + sortedNums[right])]))
-            recursiveRight(dic, left, right + 1)
-            dic[sortedNums[right]] += 1
-
-        for left in range(0, len(nums)):
-            hashNum[sortedNums[left]] -= 1
-            if fuckduplicate.get(sortedNums[left]):
+        nums.sort()
+        for i in range(0, len(nums) - 2):
+            if DP.get(nums[i]):
                 continue
             else:
-                fuckduplicate[sortedNums[left]]=True
-            recursiveRight(hashNum, left, left + 1)
-        return answer
+                DP[nums[i]] = True
+                left = i + 1
+                right = len(nums) - 1
+                while left != right:
+                    sumLR = nums[left] + nums[right]
+                    if sumLR == -nums[i]:
+                        answer.add(tuple([nums[left], nums[right], nums[i]]))
 
+                        right -= 1
+                    elif sumLR > -nums[i]:
+                        right -= 1
+
+                    elif sumLR < -nums[i]:
+                        left += 1
+        return answer
